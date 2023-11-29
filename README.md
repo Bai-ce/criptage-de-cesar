b925339be6f51c7f8fb1bf105055f0880be07be1
bai <esperanceahouissou@gmail.com>
2023-11-29 19:27:31 +0100

    modification

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # criptage-de-cesar
+on ajoute à l'entête les classes iostream et cctype 
+on declare une fonction chiffrementCesar() avec 2 pqramètre : le message à chiffrer et le decalage.
+ Pour chaque caractère,on vérifie s'il est une lettre.
+ On applique le décalage.
+ Si ce n'est pas une lettre, ajouter le caractère tel quel.
+ On demande à l'utilisateur d'entrer le message ainsi que le décalage.
+ Chiffrer le message ou déchiffrer selon le choix de l'utilisateur.
--- crypto-2.cpp
+++ crypto-2.cpp
@@ -19,24 +19,28 @@ string chiffrementCesar(const string& message, int decalage) {
 
     return resultat;
 }
-
+string dechiffrementCesar(const string& messageChiffre, int decalage) {
+    return chiffrementCesar(messageChiffre, -decalage);
+}
 int main() {
-    string message, message_chiffre ;
+    string message, message_chiffre,message_dechiffre;
     int decalage;
 
     // Demander à l'utilisateur d'entrer le message
     cout << "Entrez le message à chiffrer : ";
     cin>>message;
-
     // Demander à l'utilisateur d'entrer le décalage
     cout << "Entrez le décalage (entier) : ";
     cin >> decalage;
-
+    cout<<" choisir 1 pour chiffrer, 2 pour dechiffrer"<<endl;
     // Chiffrer le message
      message_chiffre = chiffrementCesar(message, decalage);
 
     // Afficher le message chiffré
     cout << "Message chiffré : " << message_chiffre <<endl;
 
+    message_dechiffre = dechiffrementCesar(message_chiffre,decalage);
+        cout << "Message dechiffré : " << message_dechiffre <<endl;
     return 0;
+
 }
